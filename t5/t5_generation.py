53e9cf600ec96e4dd85169134d51ce3ca496f731
Patrick von Platen <patrick.v.platen@gmail.com>
2020-03-18 16:42:41 +0100

    update

--- t5/t5_generation.py
+++ t5/t5_generation.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
-from transformers import T5WithLMHeadModel, T5Tokenizer
+from transformers import T5ForConditionalGeneration, TFT5ForConditionalGeneration, T5Tokenizer
+import sys
 
-model = T5WithLMHeadModel.from_pretrained('t5-base')
+library = sys.argv[1] if len(sys.argv) > 1 else 'pt'
+
+if library == 'pt':
+    model = T5ForConditionalGeneration.from_pretrained('t5-base')
+elif library == 'tf':
+    model = TFT5ForConditionalGeneration.from_pretrained('t5-base')
+else:
+    raise ValueError('{} does not exist. Use either "tf" or "pt"'.format(library))
+
 tok = T5Tokenizer.from_pretrained('t5-base')
-
 text = "translate English to German: How old are you?"
 
-input_ids = tok.encode(text, return_tensors='pt')
+input_ids = tok.encode(text, return_tensors=library)
 outputs = model.generate(input_ids, bos_token_id=tok.pad_token_id, max_length=22, num_beams=4, do_sample=False, early_stopping=True)
 
 print(tok.decode(outputs[0], skip_special_tokens=True))
