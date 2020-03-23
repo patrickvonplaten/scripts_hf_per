60fee02bd654f84ac8f36a78b07c1611f0c31d15
patrickvonplaten <patrick.v.platen@gmail.com>
2020-03-23 17:51:24 +0100

    update

--- t5/t5_generation.py
+++ t5/t5_generation.py
@@ -2,7 +2,8 @@
 from transformers import T5ForConditionalGeneration, TFT5ForConditionalGeneration, T5Tokenizer
 import sys
 
-library = sys.argv[1] if len(sys.argv) > 1 else 'pt'
+library = sys.argv[1]
+input_text = sys.argv[2]
 
 if library == 'pt':
     model = T5ForConditionalGeneration.from_pretrained('t5-base')
@@ -12,9 +13,11 @@ else:
     raise ValueError('{} does not exist. Use either "tf" or "pt"'.format(library))
 
 tok = T5Tokenizer.from_pretrained('t5-base')
-text = "translate English to German: How old are you?"
 
-input_ids = tok.encode(text, return_tensors=library)
-outputs = model.generate(input_ids, bos_token_id=tok.pad_token_id, max_length=22, num_beams=4, do_sample=False, early_stopping=True)
+input_ids = tok.encode(input_text, return_tensors=library)
+
+outputs = model.generate(input_ids, bos_token_id=tok.pad_token_id, max_length=200, num_beams=4, do_sample=False, early_stopping=True, length_penalty=0.6)
+
+#outputs = model.generate(input_ids, bos_token_id=tok.pad_token_id, max_length=22, num_beams=4, do_sample=False, early_stopping=True, length_penalty=0.6)
 
 print(tok.decode(outputs[0], skip_special_tokens=True))
