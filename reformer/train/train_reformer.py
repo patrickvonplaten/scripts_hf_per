9e25538b77adab48c68507d6096504801fb75820
Patrick von Platen <patrick.v.platen@gmail.com>
2020-04-22 08:26:12 +0000

    add stuff

--- reformer/train/train_reformer.py
+++ reformer/train/train_reformer.py
@@ -1,31 +1,20 @@
 #!/usr/bin/env python3
 from transformers import ReformerModelWithLMHead, ReformerTokenizer
-from transformers import ReformerConfig  # noqa: F401
+import nlp
 
 
-def get_data_set(path):
-    with open(path, 'r') as f:
-        text = f.read()
+model = ReformerModelWithLMHead.from_pretrained("patrickvonplaten/reformer-crime-and-punish")
+tokenizer = ReformerTokenizer.from_pretrained("patrickvonplaten/reformer-crime-and-punish")
 
-    # The file read above includes metadata and licensing information.
-    # For training our language model, we will only use the actual novel text.
-    start = text.find('CRIME AND PUNISHMENT')  # skip header
-    start = text.find('CRIME AND PUNISHMENT', start + 1)  # skip header
-    start = text.find('CRIME AND PUNISHMENT', start + 1)  # skip translator preface
-    end = text.rfind('End of Project')  # skip extra text at the end
-    text = text[start:end].strip()
-    return text
+model.to("cuda")
+data = nlp.load("crime_and_punish")
 
+input_text = '\n'.join(data['train']['paragraph'][:100])
 
-text = get_data_set("reformer_crime-and-punishment-2554.txt")
-model = ReformerModelWithLMHead.from_pretrained("patrickvonplaten/reformer-random")
-tokenizer = ReformerTokenizer.from_pretrained("patrickvonplaten/reformer-random")
+input_ids = tokenizer.encode(input_text, return_tensors="pt")
+input_ids = input_ids.to("cuda")
 
-# input_dict['input_ids'].shape = [1, 524288]
-input_dict = tokenizer.encode_plus(text, return_tensors='pt', pad_to_max_length=True, max_length=2**19)
+loss = model(input_ids, lm_labels=input_ids)[0]
 
-
-import ipdb
-ipdb.set_trace()
 
 pass
