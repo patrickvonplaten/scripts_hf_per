b8c96162fd437b5ade69899dfacd7f7556c3d9a5
Patrick von Platen <patrick.v.platen@gmail.com>
2020-04-23 16:52:37 +0200

    clean train script

--- reformer/train/train_reformer.py
+++ reformer/train/train_reformer.py
@@ -5,41 +5,14 @@ from transformers import (
     ReformerConfig,
     Trainer,
     DataCollator,
-    HfArgumentParser,
     TrainingArguments,
-    start_memory_tracing,
-    stop_memory_tracing
 )
 import nlp
 import torch
 
 
-def print_summary_statistics(summary):
-    print(
-        "\nLines by line memory consumption:\n"
-        + "\n".join(
-            f"{state.frame.filename}:{state.frame.line_number}: mem {state.cpu_gpu}: {state.frame.line_text}"
-            for state in summary.sequential
-        )
-    )
-    print(
-        "\nLines with top memory consumption:\n"
-        + "\n".join(
-            f"=> {state.frame.filename}:{state.frame.line_number}: mem {state.cpu_gpu}: {state.frame.line_text}"
-            for state in summary.cumulative[:6]
-        )
-    )
-    print(
-        "\nLines with lowest memory consumption:\n"
-        + "\n".join(
-            f"=> {state.frame.filename}:{state.frame.line_number}: mem {state.cpu_gpu}: {state.frame.line_text}"
-            for state in summary.cumulative[-6:]
-        )
-    )
-    print(f"\nTotal memory increase: {summary.total}")
-
-
 def create_reformer_config():
+    # define config of reformer model
     return ReformerConfig(**{
         "attention_head_size": 64,
         "attn_layers": ["local", "lsh", "local", "lsh", "local", "lsh"],
@@ -60,15 +33,33 @@ def create_reformer_config():
         "vocab_size": 320,
         "attention_probs_dropout_prob": 0.1,
         "hidden_dropout_prob": 0.05,
-        "seed": None
+        "seed": None  # that parameter is only needed for testing and will be removed soon
+    })
+
+
+def get_training_args():
+    # define the training args
+    return TrainingArguments(**{
+        "learning_rate": 5e-4,
+        "warmup_steps": 50,
+        "adam_epsilon": 1e-9,
+        "num_train_epochs": 600,
+        "output_dir": "./runs",
+        "do_train": True,
+        "do_eval": True,
+        "do_predict": True,
+        "logging_steps": 10,
+        "save_steps": 100,
     })
 
 
 def prepare_dataset(max_length):
+    # get pretrained tokenizer
     tokenizer = ReformerTokenizer.from_pretrained(
         "patrickvonplaten/reformer-crime-and-punish"
     )
 
+    # define our map function to reduce the dataset to one sample
     def flatten_and_tokenize(batch):
         all_input_text = ["".join(batch["paragraph"])]
         input_ids_dict = tokenizer.batch_encode_plus(
@@ -76,10 +67,15 @@ def prepare_dataset(max_length):
         )
         return input_ids_dict
 
-    dataset = nlp.load("crime_and_punish", split="train[:1%]")
+    # load the dataset
+    dataset = nlp.load("crime_and_punish", split="train")
+
+    # reduce the dataset
     dataset = dataset.map(
         flatten_and_tokenize, batched=True, remove_columns=["paragraph"]
     )
+
+    # prepare dataset to be in torch format
     dataset.set_format(type="torch", columns=["input_ids", "attention_mask"])
     return dataset
 
@@ -88,10 +84,12 @@ class ReformerCollator(DataCollator):
     def __init__(self, max_roll_length):
         self.max_roll_length = max_roll_length
 
+    # From the official notebook: "Normally we would have a dataset with many examples, but for this demonstration we fit a language model on the single novel only. We don't want the model to just memorize the dataset by encoding the words in its position embeddings, so at each training iteration we will randomly select how much padding to put before the text vs. after it"
     def collate_batch(self, features):
-        # randomely shift the tokens here as is done in
-        # https://colab.research.google.com/github/google/trax/blob/master/trax/models/reformer/text_generation.ipynb
+        # get random shift int
         random_shift_length = torch.randint(self.max_roll_length, (1,)).item()
+
+        # shift input and mask
         rolled_input_ids = torch.roll(
             features[0]["input_ids"], random_shift_length
         ).unsqueeze(0)
@@ -99,6 +97,7 @@ class ReformerCollator(DataCollator):
             features[0]["attention_mask"], random_shift_length
         ).unsqueeze(0)
 
+        # return dict
         return {
             "input_ids": rolled_input_ids,  # BS x SEQ_LEN
             "lm_labels": rolled_input_ids,  # BS x SEQ_LEN
@@ -106,18 +105,32 @@ class ReformerCollator(DataCollator):
         }
 
 
-def main(training_args):
-
+def main():
+    # let's use > 0.5M samples per sample
     padded_sequence_length = 2 ** 19
+
+    # reduce dataset to one example
     dataset = prepare_dataset(padded_sequence_length)
+
+    # the non_padded_sequence_length defines the max shift for our data collator
     non_padded_sequence_length = padded_sequence_length - sum(
         dataset["attention_mask"][0]
     )
-    data_collator = ReformerCollator(non_padded_sequence_length)
-    config = create_reformer_config()
 
+    # use a special data collator that randomely shifts the input_ids
+    data_collator = ReformerCollator(non_padded_sequence_length)
+
+    # create reformer config and init model
+    config = create_reformer_config()
     model = ReformerModelWithLMHead(config)
 
+    # create training params
+    training_args = get_training_args()
+
+    import ipdb
+    ipdb.set_trace()
+
+    # create the trainer
     trainer = Trainer(
         model=model,
         args=training_args,
@@ -127,13 +140,9 @@ def main(training_args):
         prediction_loss_only=True,
     )
 
-    trace = start_memory_tracing("transformers")
+    # train
     trainer.train()
-    summary = stop_memory_tracing(trace)
-    print_summary_statistics(summary)
 
 
 if __name__ == "__main__":
-    parser = HfArgumentParser((TrainingArguments))
-    training_args = parser.parse_args_into_dataclasses()[0]
-    main(training_args)
+    main()
